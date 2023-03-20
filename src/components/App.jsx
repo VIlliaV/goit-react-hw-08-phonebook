0ec95c76b9cd72fd4a86151b2214b2808a9f4dbd
Illia <illia.vasiliev@gmail.com>
2023-03-20 17:01:12 +0200

    add local

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,31 @@ import { Filter } from './Filter/Filter';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+ 
+  componentDidMount() {
+    if (localStorage.getItem('contacts')) {
+    
+      this.setState({
+      contacts: JSON.parse(localStorage.getItem('contacts')),
+    })
+  }
+  }
+  
+  componentDidUpdate(_, prevState) {
+
+    if (prevState.length === this.state.length) {
+     
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+ 
 
   changeHandler = ({ target: { value, name } }) => {
     this.setState({ [name]: value });
