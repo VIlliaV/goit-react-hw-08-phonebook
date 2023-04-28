9c3696fb978ba7b3c0fc55b84d092beb43c20cb6
Illia <illia.vasiliev@gmail.com>
2023-04-28 15:37:08 +0300

    start routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Route, Routes } from 'react-router-dom';
+
 import { Container } from './App.styled';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -6,11 +8,26 @@ import { Filter } from './Filter/Filter';
 export const App = () => {
   return (
     <Container>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter />
-      <ContactList />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login " element={<Login />} />
+          <Route path="contacts" element={<Phonebook />} />
+        </Route>
+      </Routes>
     </Container>
   );
 };
+
+// export const App = () => {
+//   return (
+//     <Container>
+//       <h1>Phonebook</h1>
+//       <ContactForm />
+//       <h2>Contacts</h2>
+//       <Filter />
+//       <ContactList />
+//     </Container>
+//   );
+// };
